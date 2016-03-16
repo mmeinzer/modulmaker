5986faa841691557f9ef2910cba65770ff56fcd2
mmeinzer <meinzer.matthew@gmail.com>
2016-03-16 03:14:04 -0400

    function wrapper

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-if (SVG.supported) {
-  main();
-} else {
-  alert('SVG not supported by browser');
-}
-
-function main() {
+(function() {
   let draw = SVG('frame').size(800,500);
   let rect = draw.rect(100, 100).attr({fill: '#f06'});
 }
-
+)();
