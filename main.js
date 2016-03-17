1a02ef559a9574f588214c82225d89a2c8478683
mmeinzer <meinzer.matthew@gmail.com>
2016-03-17 13:13:57 -0400

    adds drawing of moduls

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 (function() {
-  let draw = SVG('frame').size(800,500);
-  let rect = draw.rect(100, 100).attr({fill: '#f06'});
+  let draw = SVG('frame').size(600,500);
+  let tempgroup = draw.group().svg(tempmodul);
 }
 )();
