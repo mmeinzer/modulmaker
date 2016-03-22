662e8ce030a32c7ff123a7ec47135176bcce0f83
mmeinzer <meinzer.matthew@gmail.com>
2016-03-22 10:13:09 -0400

    add draggable lib

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 (function() {
   let draw = SVG('frame').size(600,500);
   let tempgroup = draw.group().svg(tempmodul);
+  document.getElementById("SvgjsG1007").addEventListener("drag", function(event) {
+    console.log(event.target);
+    x = event.clientX;
+    y = event.clientY;
+    console.log(x);
+  }, false);
 }
 )();
