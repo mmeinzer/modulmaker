d75cfb0fd48f78fcbdc88cc8d69abcfd60d2c385
mmeinzer <meinzer.matthew@gmail.com>
2016-03-24 21:22:19 -0400

    add drag and zoom with bounds plus frame

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 (function() {
-  let draw = SVG('frame').size("100%",500);
-  let tempgroup = draw.group().svg(tempmodul).draggable();
-}
-)();
+  var draw = new SVG('frame').size("100%", 300);
+  var border = draw.rect("100%", 300).attr({
+    fill: '#fff',
+    stroke: '#000',
+    'stroke-width': '2px'
+  });
+  var objects = draw.group();
+  objects.panZoom({
+    zoomSpeed: -5,
+    zoom: [1, 3]
+  });
+
+  var tempgroup = objects.svg(tempmodul).draggable();
+
+}());
