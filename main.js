72b20129cb24e7c04fd65d629d7f2a3c1f248dbf
mmeinzer <meinzer.matthew@gmail.com>
2016-03-24 21:54:43 -0400

    use more groups

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 (function() {
+  // create the outermost SVG element and add a border to it
   var draw = new SVG('frame').size("100%", 300);
   var border = draw.rect("100%", 300).attr({
     fill: '#fff',
     stroke: '#000',
     'stroke-width': '2px'
   });
+
+  // create an pannable zoomable object group which will contain each modul
   var objects = draw.group();
   objects.panZoom({
-    zoomSpeed: -5,
+    zoomSpeed: -7,
     zoom: [1, 3]
   });
 
-  var tempgroup = objects.svg(tempmodul).draggable();
+  // add a test modul and container g element and make it draggable
+  objects.group().draggable().svg(tempmodul);
+  // add a modul using transform method to set initial position
+  objects.group().draggable().transform({x: 50, y:50}).svg(tempmodul);
+
+  // TODO: write a function that takes a modul and adds it at a given position
 
 }());
