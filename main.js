3f06736d3674086460e805a6e2d884c57ffa49b3
mmeinzer <meinzer.matthew@gmail.com>
2016-03-30 01:54:20 -0400

    start ui and add reset(new) button

--- main.js
+++ main.js
@@ -1,24 +1,70 @@
 (function() {
   // create the outermost SVG element and add a border to it
-  var draw = new SVG('frame').size("100%", 300);
-  var border = draw.rect("100%", 300).attr({
+  var draw = new SVG('frame').size('100%', 500);
+  var border = draw.rect('100%', 500).attr({
     fill: '#fff',
     stroke: '#000',
-    'stroke-width': '2px'
+    'stroke-width': '3px'
   });
+  var moduls = makeModulsGroup()
+  var toolbox = makeToolbox()
 
-  // create an pannable zoomable object group which will contain each modul
-  var objects = draw.group();
-  objects.panZoom({
-    zoomSpeed: -7,
-    zoom: [1, 3]
-  });
+  // create an pannable zoomable object group which will contain all moduls
+  function makeModulsGroup() {
+    var moduls = draw.group();
+    moduls.panZoom({
+      zoomSpeed: -7,
+      zoom: [1, 3]
+    })
+    return moduls
+  }
 
-  // add a test modul and container g element and make it draggable
-  objects.group().draggable().svg(tempmodul);
-  // add a modul using transform method to set initial position
-  objects.group().draggable().transform({x: 50, y:50}).svg(tempmodul);
+  function resetWindow() {
+    if (moduls) {
+      moduls.each(function(i, children) {
+        this.remove()
+      })
+    }
+    moduls
+      .transform({
+        scale: 1
+      })
+      .transform({
+        x: 0
+      })
+      .transform({
+        y: 0
+      })
+  }
 
-  // TODO: write a function that takes a modul and adds it at a given position
+  // make the toolbox overlay
+  function makeToolbox() {
+    var ui = draw.group();
+    var uiBox = ui.rect('15%', 350).move('83%', '13%')
+      .radius(10)
+      .attr({
+        fill: '#19C3FF'
+      })
+      // drop shadow
+      .filter(function(add) {
+        var blur = add.offset(2, 2).in(add.sourceAlpha).gaussianBlur(4)
+        add.blend(add.source, blur)
+          // not sure what this line does... may not need
+        this.size('175%', '175%').move('-50%', '-50%')
+      });
+    return ui
+  }
 
+  // function for adding a modul to the object group
+  function placeModul() {
+    // TODO: the button id could be set to the modul name eg.
+    // var modulType = this.id;
+
+    // TODO: this would likely be better implemented with use()
+    moduls.group().draggable().svg(tempmodul);
+  }
+
+  // event listeners for clicks
+  toolbox.node.addEventListener('click', placeModul);
+  document.getElementById("newButton").addEventListener('click', resetWindow);
 }());
