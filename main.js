31980173de78005121dc0ac81b8e76b3e4514d45
Matthew Meinzer <meinzer.matthew@gmail.com>
2016-04-25 01:47:32 -0400

    update working plan and the moduldefs

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 
 (function() {
   // create the outermost SVG element and add a border to it
-  var draw = new SVG('frame').size('100%', 500);
+  var draw = new SVG('frame').size('100%', 500)
   var border = draw.rect('100%', 500).attr({
     fill: '#fff',
     stroke: '#000',
     'stroke-width': '3px'
-  });
+  })
   var moduls = makeModulsGroup()
   var toolbox = makeToolbox()
   var clickState = {
@@ -18,9 +18,8 @@
     endY: null   
   }
   
-  // create an pannable zoomable object group which will contain all moduls
-  function makeModulsGroup() {
-    var moduls = draw.group();
+  function makeModulsGroup() { // create an pannable zoomable object group which will contain all moduls
+    var moduls = draw.group()
     moduls.panZoom({
       zoomSpeed: -1,  
       zoom: [1, 3]
@@ -46,9 +45,8 @@
       })
   }
 
-  // make the toolbox overlay
   function makeToolbox() {
-    var ui = draw.group();
+    var ui = draw.group()
     var uiBox = ui.rect('15%', 350).move('83%', '13%')
       .radius(10)
       .attr({
@@ -59,33 +57,37 @@
         var blur = add.offset(2, 2).in(add.sourceAlpha).gaussianBlur(4)
         add.blend(add.source, blur)
         this.size('175%', '175%').move('-50%', '-50%')
-      });
+      })
     return ui
   }
 
-  function addModul() {
-    // TODO: the button id could be set to the modul name eg.
-    // var modulType = this.id
-    
+  function addModul() {    
     // TODO: this would likely be better implemented with use()
-    // adds the tempmodul
-    var modul = moduls.group().draggable().svg(tempmodul);
-    
+    var modul = moduls.group().draggable().svg(tempmodul) // adds the tempmodul
     modul.click(function(event) {
       if (event.target.className.baseVal == "header") {
+        console.log("1. First click? ", clickState.firstClick)
         if (clickState.firstClick){
           clickState.startX = parseFloat(event.target.getAttribute('x')) + this.transform('x') + parseFloat(event.target.getAttribute('width'))/2
           clickState.startY = parseFloat(event.target.getAttribute('y')) + this.transform('y') + parseFloat(event.target.getAttribute('height'))/2
         } else {
           clickState.endX = parseFloat(event.target.getAttribute('x')) + this.transform('x') + parseFloat(event.target.getAttribute('width'))/2
           clickState.endY = parseFloat(event.target.getAttribute('y')) + this.transform('y') + parseFloat(event.target.getAttribute('height'))/2
+          var controlPoint = {
+            x: ((clickState.startX + clickState.endX) / 2),
+            y: (((clickState.startY + clickState.endY) / 2) + Math.abs(clickState.startY - clickState.endY))
+          }
+          console.log(controlPoint)
           var startHeader = "M " + clickState.startX + " " + clickState.startY + " "
-          var endHeader = "C 100 100 400 100 " + clickState.endX + " " + clickState.endY
+          var endHeader = "Q " + controlPoint.x + " " + controlPoint.y + " " + clickState.endX + " " + clickState.endY
           var pathString = startHeader + endHeader
           moduls.group().path(pathString).fill('none').stroke({color: '#f00', width: 1.5})
         }
-        clickState.firstClick ? clickState.firstClick = false : clickState.firstClick = true
+        clickState.firstClick ? clickState.firstClick = false : clickState.firstClick = true // toggle click state
       }
+    })
+    // TODO: Implement update function for connected jumpers
+    modul.on('dragmove', function(){
     })
   }
   
@@ -101,6 +103,6 @@
   }
   
   // event listeners for clicks
-  toolbox.on('click', addModul, false);
-  document.getElementById('newButton').addEventListener('click', resetWindow, false);
+  toolbox.on('click', addModul, false)
+  document.getElementById('newButton').addEventListener('click', resetWindow, false)
 }());
