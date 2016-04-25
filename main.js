eea7c3f60506868acad2ead787ae0098800d0e49
Matthew Meinzer <meinzer.matthew@gmail.com>
2016-04-25 01:10:54 -0400

    adds click state to connect two header pins

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 (function() {
   // create the outermost SVG element and add a border to it
   var draw = new SVG('frame').size('100%', 500);
@@ -8,7 +10,14 @@
   });
   var moduls = makeModulsGroup()
   var toolbox = makeToolbox()
-
+  var clickState = {
+    firstClick: true,
+    startX: null,
+    startY: null,
+    endX: null,
+    endY: null   
+  }
+  
   // create an pannable zoomable object group which will contain all moduls
   function makeModulsGroup() {
     var moduls = draw.group();
@@ -64,20 +73,30 @@
     
     modul.click(function(event) {
       if (event.target.className.baseVal == "header") {
-        svgElem = this
-        addJumper(event, svgElem)
+        if (clickState.firstClick){
+          clickState.startX = parseFloat(event.target.getAttribute('x')) + this.transform('x') + parseFloat(event.target.getAttribute('width'))/2
+          clickState.startY = parseFloat(event.target.getAttribute('y')) + this.transform('y') + parseFloat(event.target.getAttribute('height'))/2
+        } else {
+          clickState.endX = parseFloat(event.target.getAttribute('x')) + this.transform('x') + parseFloat(event.target.getAttribute('width'))/2
+          clickState.endY = parseFloat(event.target.getAttribute('y')) + this.transform('y') + parseFloat(event.target.getAttribute('height'))/2
+          var startHeader = "M " + clickState.startX + " " + clickState.startY + " "
+          var endHeader = "C 100 100 400 100 " + clickState.endX + " " + clickState.endY
+          var pathString = startHeader + endHeader
+          moduls.group().path(pathString).fill('none').stroke({color: '#f00', width: 1.5})
+        }
+        clickState.firstClick ? clickState.firstClick = false : clickState.firstClick = true
       }
     })
   }
   
   // jumper will be an svg path
   // need to form this path as a string
-  function addJumper(event, svgElem) {
-    startX = parseFloat(event.target.getAttribute('x')) + svgElem.transform('x') + parseFloat(event.target.getAttribute('width'))/2
-    startY = parseFloat(event.target.getAttribute('y')) + svgElem.transform('y') + parseFloat(event.target.getAttribute('height'))/2
-    startHeader = "M " + startX + " " + startY
-    endHeader = " C 100 100 400 100 400 200"
-    pathString = startHeader + endHeader
+  function addJumper(event, svgElem, click) {
+    var startX = parseFloat(event.target.getAttribute('x')) + svgElem.transform('x') + parseFloat(event.target.getAttribute('width'))/2
+    var startY = parseFloat(event.target.getAttribute('y')) + svgElem.transform('y') + parseFloat(event.target.getAttribute('height'))/2
+    var startHeader = "M " + startX + " " + startY
+    var endHeader = " C 100 100 400 100 400 200"
+    var pathString = startHeader + endHeader
     moduls.group().path(pathString).fill('none').stroke({color: '#f00', width: 1.5})
   }
   
